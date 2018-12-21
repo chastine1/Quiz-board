47c2c147a420ba9dce55a7b55f82149e11bf1e90
chastine1 <batamurizashastine@gmail.com>
2018-12-21 11:35:15 +0200

    save the codes

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-$document.ready(function(){
-    $("button#submit").click(fucntion());
-    {
-        $("h3").append("<h3>submit</h3>");
-    }
-    $(document).ready(function() {
-        $("button#green").click(function() {
-          $("h3").addClass("green-background");
+
+
+
+        $(document).ready(function() {
+            $("#blank form").submit(function() {
+                $(".name").append("name");
+                $(".enter email").append("email");
+        
+                $("#story").show();
+        
+                event.preventDefault();
+            });
         });
