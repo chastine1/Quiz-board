a6dffeb5ed91828ad4ca4ac795be8479e511be22
chastine1 <batamurizashastine@gmail.com>
2018-12-21 14:56:57 +0200

    save the js codes

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
-var flavor = $("input:radio[name=chastine]:checked").val();
+var chastine = $("input:radio[name=chastine]:checked").val();
+
+var lull = $("input:radio[name=lull]:checked").val();
+
+var gaz = $("input:radio[name=gaz]:checked").val();
+
+var tree = $("input:radio[name=tree]:checked").val();
+
+$(document).ready(function() {
+    $("#blanks form").submit(function(event) {
+      var chastine= $("input#chastine").val()
+      var lull= $("input#lull").val()
+      var gaz= $("input#gaz").val()
+      var tree= $("input#tree").val()
+
+      $(".names").text(namesInput);
+      $(".email").text(emailInput);
+      
+      
+
+      $("#story").show();
+
+      event.preventDefault();
+    });
+  });
+
+
+
 
 
         $(document).ready(function() {
-            $("#blank form").submit(function() {
+            $("").submit(function() {
                 $(".name").append("name");
-                $(".enter email").append("email");
+                $(".name").prepand("name");
         
                 $("#story").show();
         
