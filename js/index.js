106a10a7a06df053a4dec3476e8df6d5104d43a1
chastine1 <batamurizashastine@gmail.com>
2019-01-14 12:01:06 +0200

    save the  changes

--- js/index.js
+++ js/index.js
@@ -1,90 +1,36 @@
-var coloo=0
-
-if (chastine === "10") {
-   chastine= chastine + 10;
-};
-if (lull === "10") {
-   lull = lull+ 10;
-};
-if (gaz  === "10") {
-   gaz = gaz + 10;
-};
-if ( tree === "10") {
-   tree = tree + 10;
-};
-
-if (container === "40" && container === "complete"){
-  alert("submitted ! ")  
-}
-else if(container !== "40"){
-    alert("failed ! ")
-}
 
 
 
+      function leTriangle() {
+        var chastine = document.getElementById("chastine").value;
+        var lull= document.getElementById("lull").value;
+        var gaz= document.getElementById("gaz").value;
+        var tree = document.getElementById("tree").value;
 
-$(".container").show();
-$("#correct").text("Your Total Marks: " + container);
-
-event.preventDefault();
-
-};
-
-$(document).ready(function (){
-   $('#form board').submit (function(event){
-
-
-var option = parseInt($("input:radio[name=chastine]:checked ").val());
-var optionOne =parseInt( $("input:radio[name=lull]:checked").val());
-var optionTwo =parseInt( $("input:radio[name=gaz]:checked").val());
-var optionThree =parseInt( $("input:radio[name=tree]:checked").val());
-
-var opt = 0;
-
-Message Input
+      if(chastine === '' || lull === '' || gaz === '' || tree === '') {
+        alert("Please enter the answers");
+      }
 
 
-Message Divine Umurerwa
-
-var chastine = $("input:radio[name=chastine]:checked").val();
-
-var lull = $("input:radio[name=lull]:checked").val();
-
-var gaz = $("input:radio[name=gaz]:checked").val();
-
-var tree = $("input:radio[name=tree]:checked").val();
-
-      var names= $(".names").text(namesInput);
-      var email= $(".email").text(emailInput);
-      
-      
-
-      $("#story").show();
-
-      event.preventDefault();
-    });
-  });
-
-
-
-
-
-        $(document).ready(function() {
-            $("").submit(function() {
-                $(".name").append("name");
-                $(".name").prepand("name");
-        
-                $("#story").show();
-        
-                event.preventDefault();
-            });
+      }
+      var testResults = function(one, two , three ,four){
+        return one + two + three + four ;
+        }
+        $(document).ready(function(){
+     
+          $("form#test").submit(function(event){
+            var chastine = parseInt($("input:radio[name=chastine]:checked").val());
+            var lull = parseInt($("input:radio[name=lull]:checked").val());
+            var gaz = parseInt($("input:radio[name=gaz]:checked").val());
+            var tree = parseInt($("input:radio[name=tree]:checked").val());
+            
+     
+            var result = testResults(chastine,lull,gaz,tree);
+     
+            $("#display").text("Your score is:" + result);
+     
+           event.preventDefault();
+            $(".final").show();
+          });
         });
-
-     var answers= $("aswers#hide").click(function(){
-        $("p").hide();
-      });
-      
-      $("#show").click(function(){
-        $("p").show();
-      });   
 
